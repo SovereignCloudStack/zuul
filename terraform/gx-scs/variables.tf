a9c6c046646ec2ab0e179e81e0120d24f070c60a
Oliver Kautz <oliver.kautz@gonicus.de>
2024-01-17 15:25:49 +0100

    Add Terraform Config to deploy dev resources
    Signed-off-by: Oliver Kautz <oliver.kautz@gonicus.de>

--- terraform/gx-scs/variables.tf
+++ terraform/gx-scs/variables.tf
@@ -1,3 +1,15 @@
+variable "zuul_vm_name" {
+  type        = string
+  description = "Name of the Compute Instance for zuul. Default: zuul"
+  default     = "zuul"
+}
+
+variable "zuul_cloudsyaml_identifier" {
+  type        = string
+  description = "Identitifier of the OPenStack Project to deploy the resources to"
+  default     = "gx-scs_zuul"
+}
+
 variable "zuul_network_name" {
   type        = string
   description = "The name of the network that is created for zuul deployment"
@@ -45,4 +57,26 @@ variable "zuul_inital_ssh_pubkey" {
   description = "key name for the zuul instance"
   nullable    = false
   default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC5TgWQ5ZKRkNd/We/laApQgresUqbJTABtQd2q0i/sMMHJJOSBzSW8EbC+4jTbbL4ui0GFfXfYwLHve0ZzMBL4cso5cKKv00GFRwHxyYE/ACiYzZDJLEo9uygvfELRUZF8/qRklJO4sIag2diR36vg6IYGsd+o/Z2a1WQS3SUCGzjHupG1bN+bLY7749q/s7WU22M5rnX61Qvu/0jDFxmpuxGb47fOWIUoGTAXA+OkUN3WZ0p1MXNJSHS9YY5XKZSeOQGcmj0htu9BVKa1UkBDEuxyh3PbY7qVf2NPDO0Y/a1TpTI6DKvYd3Km6HwXVy5GY84CauXr8X3GCye6wSL515eXs9xsaDHjVid+5AyCUr70jyYKOL6WIi058L2w+0pvZ3Ujn47TNnzhFuq65+Lx1J8ICjG4lMIWHvYcpBa7ec+4BbLsqAIi763an/v4rRum5FigMhd3di+ecvVliiIOj3p8zH5pmv+l6YzzN2ryypr3RoflVhRqjAi5SxeC2iBvpxlC1FDTKhLCprig7ZKr+mp17CFL+odhVG+3mSPISSeUDUVprqAb3Pem81mK4bkW6uQzgZOju8tHTfhh/N0SXHJLe3gol3fc4r2O4AWlZVsN12A3hHE0UXD8D+FrrCxtb/ssicc0AJn6/kVTWCSdRdvG2MaNzLMi34uBoKtp5Q== @o-otte"
+}
+
+## dev overwrites
+variable "zuul_cloudsyaml_identifier_dev" {
+  type        = string
+  description = "Identitifier of the OPenStack Project to deploy the resources to"
+  default     = "gx-scs_gonicus"
+}
+
+variable "zuul_flavor_name_dev" {
+  type        = string
+  description = "flavor name for the zuul instance"
+  nullable    = false
+  default     = "SCS-4V-16"
+}
+
+# Router ID in Gonicus Project
+variable "router_id_dev" {
+  type        = string
+  description = "router the zuul network should be attached to"
+  nullable    = false
+  default     = "0ec38c02-c4dc-4d62-ad26-1fed240fed2f"
 }
