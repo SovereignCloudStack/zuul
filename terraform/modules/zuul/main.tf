16ed92e10a21baf1d901e9d2bb9685e8fe1bb272
Oliver Kautz <oliver.kautz@gonicus.de>
2024-01-30 15:34:11 +0100

    Add http and https ports to Zuul Security Group
    Signed-off-by: Oliver Kautz <oliver.kautz@gonicus.de>

--- terraform/modules/zuul/main.tf
+++ terraform/modules/zuul/main.tf
@@ -66,6 +66,26 @@ resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_zuul_http" {
   security_group_id = openstack_networking_secgroup_v2.zuul_secgroup.id
 }
 
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_http" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.zuul_secgroup.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_https" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 443
+  port_range_max    = 443
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.zuul_secgroup.id
+}
+
 # create zuul head node
 resource "openstack_compute_instance_v2" "zuul" {
   name            = var.zuul_vm_name
